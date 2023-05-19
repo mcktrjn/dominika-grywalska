7d19ff2ee3c8b32568980e490ecfc4da06eecd8a
mcktrjn <maciektrojan248@gmail.com>
2023-05-19 23:56:29 +0200

    refactor typography component

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -1,106 +1,52 @@
 import cx from "classnames";
-import React from "react";
-import { colors } from "../../constants";
+import { useEffect, useRef, useState } from "react";
+import { NBSP, ZWSP, colors } from "../../constants";
 import { Color } from "../../types";
 import styles from "./Typography.module.scss";
 
 type Props = {
   className?: string;
-  tag: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p";
-  color?: Color;
-  children: React.ReactNode;
-} & (
-  | {
-      isFamilySerif?: true;
-      weight?: 400 | 500 | 600 | 700;
-    }
-  | {
-      isFamilySerif?: false;
-      weight?: 300 | 400 | 500 | 600 | 700;
-    }
-);
-
-type AdditionalProps = {
-  Decoration: React.FC<DecorationProps>;
-  Animation: React.FC<AnimationProps>;
-};
-
-export const Typography: React.FC<Props> & AdditionalProps = ({
-  className,
-  tag,
-  isFamilySerif,
-  weight,
-  color,
-  children,
-}) => {
-  const familyClassName = { [styles["family-serif"]]: isFamilySerif };
-  const weightClassName = styles[`weight-${weight}`];
-  const componentClassName = cx(familyClassName, weightClassName, className);
-
-  const headingColor = colors[color || "neutral900"];
-  const paragraphColor = colors[color || "neutral700"];
-
-  let colorProperty;
-  if (tag !== "p") {
-    colorProperty = headingColor;
-  } else {
-    colorProperty = paragraphColor;
-  }
-
-  return React.createElement(
-    tag,
-    {
-      className: componentClassName || undefined,
-      style: { color: colorProperty },
-    },
-    children
-  );
-};
-
-type DecorationProps = {
+  isVisible?: boolean;
+  variant: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p";
+  isFamilyPlayfairDisplay?: boolean;
+  weight?: 400 | 500 | 600 | 700;
   color?: Color;
   decoration?: "background" | "underline";
   decorationColor?: Color;
+  decorationTextColor?: Color;
+  decorationStart?: number;
+  decorationEnd?: number;
   children: string;
 };
 
-const Decoration: React.FC<DecorationProps> = ({
-  color = "success300",
+export const Typography: React.FC<Props> = ({
+  className,
+  isVisible,
+  variant,
+  isFamilyPlayfairDisplay,
+  weight,
+  color,
   decoration = "background",
   decorationColor = "success100",
-  children,
-}) => {
-  const colorProperty = colors[color];
-  const backgroundColor = colors[decorationColor];
-  const backgroundImageProperty = `linear-gradient(${backgroundColor}, ${backgroundColor})`;
-
-  return (
-    <span
-      className={styles[`decoration-${decoration}`]}
-      style={{ color: colorProperty, backgroundImage: backgroundImageProperty }}
-    >
-      {children}
-    </span>
-  );
-};
-
-Typography.Decoration = Decoration;
-
-type AnimationProps = DecorationProps & {
-  isVisible: boolean;
-  decorationStart?: number; // TODO: fix typing to make "decorationStart" and "decorationEnd" props required if "decoration" is true
-  decorationEnd?: number;
-};
-
-const Animation: React.FC<AnimationProps> = ({
-  isVisible,
-  color,
-  decoration,
-  decorationColor,
+  decorationTextColor = "success300",
   decorationStart,
   decorationEnd,
   children,
 }) => {
+  const Tag = variant;
+  const componentClassName = cx(
+    {
+      [styles.visible]: isVisible,
+      [styles["family-playfairDisplay"]]: isFamilyPlayfairDisplay,
+    },
+    styles[`weight-${weight}`],
+    className
+  );
+
+  const headingColor = colors[color || "neutral900"];
+  const paragraphColor = colors[color || "neutral700"];
+  const colorProperty = variant !== "p" ? headingColor : paragraphColor;
+
   const words = children.split(" ");
   const getDecorationRange = (index: number) => {
     if (decorationStart !== undefined && decorationEnd !== undefined) {
@@ -108,31 +54,52 @@ const Animation: React.FC<AnimationProps> = ({
     }
   };
 
-  const componentClassName = cx(styles.animation, {
-    [styles.visible]: isVisible,
-  });
+  const spaceRef = useRef<HTMLSpanElement>(null);
+  const [spaceWidth, setSpaceWidth] = useState(0);
+
+  useEffect(() => {
+    if (spaceRef.current) {
+      setSpaceWidth(spaceRef.current.getBoundingClientRect().width);
+    }
+  }, []);
 
   return (
-    <>
-      {words.map((word, index) =>
-        getDecorationRange(index) ? (
-          <span key={index} className={componentClassName}>
-            <Typography.Decoration
-              color={color}
-              decoration={decoration}
-              decorationColor={decorationColor}
-            >
-              {word}
-            </Typography.Decoration>
+    <Tag
+      className={componentClassName || undefined}
+      style={{ color: colorProperty }}
+    >
+      {["h1", "h2", "h3"].includes(variant) ? (
+        <>
+          {words.map((word, index) =>
+            getDecorationRange(index) ? (
+              <span key={index} className={styles.animation}>
+                <span
+                  className={styles[`decoration-${decoration}`]}
+                  style={{
+                    padding: `0 ${spaceWidth / 2}px`,
+                    backgroundImage: `linear-gradient(${colors[decorationColor]}, ${colors[decorationColor]})`,
+                    color: colors[decorationTextColor],
+                  }}
+                >
+                  {word + ZWSP}
+                </span>
+              </span>
+            ) : (
+              <span key={index} className={styles.animation}>
+                <span> {word} </span>
+              </span>
+            )
+          )}
+          <span
+            ref={spaceRef}
+            style={{ position: "absolute", userSelect: "none" }}
+          >
+            {NBSP}
           </span>
-        ) : (
-          <span key={index} className={componentClassName}>
-            <span> {word} </span>
-          </span>
-        )
+        </>
+      ) : (
+        children
       )}
-    </>
+    </Tag>
   );
 };
-
-Typography.Animation = Animation;
